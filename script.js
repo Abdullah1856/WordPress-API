95e033aa8e82eaf9aeaebda4c3d381ac298a92d3
Abdullah <68245138+Abdullah1856@users.noreply.github.com>
2023-08-15 10:59:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+const endpoint = 'https://testwp1.braincrop.net/wp-json/wp/v2/posts';
 
+fetch(endpoint)
+  .then(response => response.json())
+  .then(data => {
+    const postContainer = document.getElementById('post-container');
+
+    data.forEach(post => {
+      const box = document.createElement('a');
+      box.classList.add('post-box');
+      box.href = post.link;
+
+      const image = document.createElement('img');
+      if (post.featured_media !== 0) {
+        fetch(`https://testwp1.braincrop.net/wp-json/wp/v2/media/${post.featured_media}`)
+          .then(response => response.json())
+          .then(mediaData => {
+            image.src = mediaData.source_url;
+          });
+      }
+
+      const title = document.createElement('h2');
+      title.innerHTML = post.title.rendered;
+
+      const excerpt = document.createElement('p');
+      excerpt.innerHTML = post.excerpt.rendered;
+
+      box.appendChild(image);
+      box.appendChild(title);
+      box.appendChild(excerpt);
+
+      postContainer.appendChild(box);
+    });
+  })
+  .catch(error => console.error('Error fetching data:', error));
