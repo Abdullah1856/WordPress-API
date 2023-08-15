4b7729e997f239bb612120b43d16e9e8bbb70238
Abdullah <68245138+Abdullah1856@users.noreply.github.com>
2023-08-15 10:49:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,37 +3,33 @@ const endpoint = 'https://testwp1.braincrop.net/wp-json/wp/v2/posts';
 fetch(endpoint)
   .then(response => response.json())
   .then(data => {
-    const postContainer = document.getElementById('post-container'); // Assuming you have a div with this ID
+    const postContainer = document.getElementById('post-container');
 
-data.forEach(post => {
-  const box = document.createElement('div');
-  box.classList.add('post-box'); // You can style this class in CSS for formatting
+    data.forEach(post => {
+      const box = document.createElement('a');
+      box.classList.add('post-box');
+      box.href = post.link;
 
-  const image = document.createElement('img');
-  if (post.featured_media !== 0) {
-    fetch(`https://testwp1.braincrop.net/wp-json/wp/v2/media/${post.featured_media}`)
-      .then(response => response.json())
-      .then(mediaData => {
-        image.src = mediaData.source_url;
-      });
-  }
+      const image = document.createElement('img');
+      if (post.featured_media !== 0) {
+        fetch(`https://testwp1.braincrop.net/wp-json/wp/v2/media/${post.featured_media}`)
+          .then(response => response.json())
+          .then(mediaData => {
+            image.src = mediaData.source_url;
+          });
+      }
 
-  const title = document.createElement('h2');
-  title.innerHTML = post.title.rendered;
+      const title = document.createElement('h2');
+      title.innerHTML = post.title.rendered;
 
-  const excerpt = document.createElement('p');
-  excerpt.innerHTML = post.excerpt.rendered;
+      const excerpt = document.createElement('p');
+      excerpt.innerHTML = post.excerpt.rendered;
 
-  const link = document.createElement('a');
-  link.href = post.link;
-  link.innerHTML = 'Read More';
+      box.appendChild(image);
+      box.appendChild(title);
+      box.appendChild(excerpt);
 
-  box.appendChild(title);
-  box.appendChild(excerpt);
-  box.appendChild(image);
-  box.appendChild(link);
-
-  postContainer.appendChild(box);
-});
+      postContainer.appendChild(box);
+    });
   })
   .catch(error => console.error('Error fetching data:', error));
